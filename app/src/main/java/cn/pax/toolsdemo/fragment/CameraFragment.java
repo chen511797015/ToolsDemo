9788792fa39593cc44b13468c6deff94c98895e1
chen511797015 <chen511797015@126.com>
2017-01-11 11:22:18 +0800

    添加相机界面

---
+++ app/src/main/java/cn/pax/toolsdemo/fragment/CameraFragment.java
@@ -0,0 +1,35 @@
+package cn.pax.toolsdemo.fragment;
+
+import cn.pax.toolsdemo.R;
+import cn.pax.toolsdemo.base.BaseFragment;
+
+/**
+ * Created by chendd on 2017/1/11.
+ */
+
+public class CameraFragment extends BaseFragment {
+    @Override
+    protected int setView() {
+        return R.layout.fragment_camera;
+    }
+
+    @Override
+    protected void findView() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initEvent() {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}
--- app/src/main/java/cn/pax/toolsdemo/fragment/SystemFragment.java
+++ app/src/main/java/cn/pax/toolsdemo/fragment/SystemFragment.java
@@ -1,7 +1,18 @@
 package cn.pax.toolsdemo.fragment;
 
+import android.graphics.Camera;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.pax.toolsdemo.R;
+import cn.pax.toolsdemo.adapter.SystemPagerAdapter;
 import cn.pax.toolsdemo.base.BaseFragment;
+
+import static android.R.attr.tag;
 
 /**
  * Created by chendd on 2017/1/11.
@@ -9,6 +20,14 @@ import cn.pax.toolsdemo.base.BaseFragment;
  */
 
 public class SystemFragment extends BaseFragment {
+    private static final String TAG = "SystemFragment";
+
+    ViewPager mViewPager;
+    TabLayout mTabLayout;
+
+    List<Fragment> mList;
+
+
     @Override
     protected int setView() {
         return R.layout.fragment_system;
@@ -17,10 +36,22 @@ public class SystemFragment extends BaseFragment {
     @Override
     protected void findView() {
 
+        mViewPager = (ViewPager) mView.findViewById(R.id.vp_system);
+        mTabLayout = (TabLayout) mView.findViewById(R.id.tb_system);
+
     }
 
     @Override
     protected void initView() {
+
+        mList = new ArrayList<>();
+        CameraFragment mCameraFragment = new CameraFragment();
+        mList.add(mCameraFragment);
+
+
+        SystemPagerAdapter mSystemPagerAdapter = new SystemPagerAdapter(getFragmentManager(), mList);
+        mViewPager.setAdapter(mSystemPagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
 
     }
 
