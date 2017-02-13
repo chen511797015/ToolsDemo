b445b6459b70ac4eb1459dba834fc3a81ec05624
chen511797015 <chen511797015@126.com>
2017-02-13 16:50:45 +0800

    优化

--- screenadapter/src/main/java/com/pax/screenadapter/MainActivity.java
+++ screenadapter/src/main/java/com/pax/screenadapter/MainActivity.java
@@ -2,12 +2,22 @@ package com.pax.screenadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindow().getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Log.e(TAG, "widthPixels: " + dm.widthPixels);
+        Log.e(TAG, "heightPixels: " + dm.heightPixels);
+        Log.e(TAG, "densityDpi: " + dm.densityDpi);
+
     }
 }
